5a5a1cb2f6cac3e9c9d8e4d907d3fa2ebef72d27
Sanyog Mane <94354794+sanyog12@users.noreply.github.com>
2024-05-12 19:58:02 +0530

    Add files via upload

---
+++ fullstack-front/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './layout/Navbar';
+import Home from './pages/Home';
+import axios from 'axios';
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <Home></Home>
+    </div>
+  );
+}
+
+export default App;
---
+++ fullstack-front/src/pages/Home.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+export default function Home() {
+
+    const[users,setUsers] = useState([]);
+    useEffect(()=>{
+       loadUsers();
+    },[])
+
+    const loadUsers=async()=>{
+        const result = await axios.get("http://localhost:8080/users");
+        setUsers(result.data)
+    }
+  return (
+    <div className="container">
+      <div className="py-4">
+        <table className="table border shadow">
+          <thead>
+            <tr>
+              <th scope="col">Id</th>
+              <th scope="col">Name</th>
+              <th scope="col">Username</th>
+              <th scope="col">Email</th>
+              <th scope="col">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+                users.map((users,index)=>(
+                    <tr>
+                    <th scope="row" key={index}>{index+1}</th>
+                    <td>{users.name}</td>
+                    <td>{users.username}</td>
+                    <td>{users.email}</td>
+                  </tr>
+                ))
+            }
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
